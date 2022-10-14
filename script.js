8969065b749fa7fb24c658ccebaad57b9abc8036
Aldo Vizcarra <avizcarra87@gmail.com>
2022-10-13 19:38:07 -0700

    Edit text color

--- script.js
+++ script.js
@@ -26,6 +26,15 @@ function playRound(e) {
   para2.classList.add('message-results');
   const messageResults = document.querySelector('.message-results');
 
+  const span1 = document.createElement('span');
+  const span2 = document.createElement('span');
+  const span3 = document.createElement('span');
+  const span4 = document.createElement('span');
+
+  span2.classList.add('span-red');
+  span4.classList.add('span-red');
+  const spanArr = [span1, span2, span3, span4];
+
   if (
     (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
     (playerSelection === 'Paper' && computerSelection === 'Rock') ||
@@ -37,14 +46,14 @@ function playRound(e) {
         score.parentNode.removeChild(score);
         messageResults.parentNode.removeChild(messageResults);
         if (playerCount === 5) {
-          para2.textContent = `You are the Winner!`;
+          para2.textContent = `You Are The Winner!`;
           para2.style.fontSize = '2.5rem';
           results.appendChild(para2);
           playerCount = 0;
           computerCount = 0;
           return;
         } else if (computerCount === 5) {
-          para2.textContent = `You Lose! The Computer beat you`;
+          para2.textContent = `You Lose! The Computer Beat You!`;
           para2.style.fontSize = '2.5rem';
           results.appendChild(para2);
           playerCount = 0;
@@ -56,8 +65,12 @@ function playRound(e) {
       }
     }
 
-    para.textContent = `Player 1: ${playerCount} Computer: ${computerCount}`;
-    para2.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    span1.textContent = `You: `;
+    span2.textContent = `${playerCount}`;
+    span3.textContent = ` Computer: `;
+    span4.textContent = `${computerCount}`;
+    spanArr.forEach((span) => para.appendChild(span));
+    para2.textContent = `You Win! ${playerSelection} Beats ${computerSelection}!`;
     results.appendChild(para);
     results.appendChild(para2);
   } else if (
@@ -71,14 +84,14 @@ function playRound(e) {
         score.parentNode.removeChild(score);
         messageResults.parentNode.removeChild(messageResults);
         if (playerCount === 5) {
-          para2.textContent = `You are the Winner!`;
+          para2.textContent = `You Are The Winner!`;
           para2.style.fontSize = '2.5rem';
           results.appendChild(para2);
           playerCount = 0;
           computerCount = 0;
           return;
         } else if (computerCount === 5) {
-          para2.textContent = `You Lose! The Computer beat you`;
+          para2.textContent = `You Lose! The Computer Beat You!`;
           para2.style.fontSize = '2.5rem';
           results.appendChild(para2);
           playerCount = 0;
@@ -90,8 +103,12 @@ function playRound(e) {
       }
     }
 
-    para.textContent = `Player 1: ${playerCount} Computer: ${computerCount}`;
-    para2.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    span1.textContent = `You: `;
+    span2.textContent = `${playerCount}`;
+    span3.textContent = ` Computer: `;
+    span4.textContent = `${computerCount}`;
+    spanArr.forEach((span) => para.appendChild(span));
+    para2.textContent = `You Lose! ${computerSelection} Beats ${playerSelection}!`;
     results.appendChild(para);
     results.appendChild(para2);
   } else if (
@@ -108,8 +125,12 @@ function playRound(e) {
       }
     }
 
-    para.textContent = `Player 1: ${playerCount} Computer: ${computerCount}`;
-    para2.textContent = `It's a Tie!`;
+    span1.textContent = `You: `;
+    span2.textContent = `${playerCount}`;
+    span3.textContent = ` Computer: `;
+    span4.textContent = `${computerCount}`;
+    spanArr.forEach((span) => para.appendChild(span));
+    para2.textContent = `It's A Tie!`;
     results.appendChild(para);
     results.appendChild(para2);
   }
